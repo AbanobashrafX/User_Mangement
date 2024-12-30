b430377e1c48b1f36d45b40e1b2593a09a39d9ef
AbanobAshrafX <abanobashraf07@gmail.com>
2024-12-30 02:17:16 +0200

    Add BaseModel for common timestamp fields and update User model to inherit from it

--- services/users/models.py
+++ services/users/models.py
@@ -1,18 +1,23 @@
 import uuid
 
-from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+
+from services.common.models import BaseModel, models
 
 
-class User(AbstractUser):
+class User(BaseModel, AbstractUser, PermissionsMixin):
     # Add any custom fields here, e.g.,
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     username = models.CharField(max_length=20, blank=True, unique=True)
     password = models.CharField(max_length=255, blank=False)
-    
-    
+
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
 
     def __str__(self):
         return self.get_full_name() or self.username
